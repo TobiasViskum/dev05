361d751e81ff5ddc2cf4654aab8b47b2991fe35a
TobiasViskum <tobias.t.viskum@gmail.com>
2023-06-21 13:31:24 +0200

    commit

--- src/components/page-index/SpinningIcon.tsx
+++ src/components/page-index/SpinningIcon.tsx
@@ -23,7 +23,7 @@ export default function SpinningIcon() {
           ></path>
         </svg>
       </div>
-      <div className="fixed bottom-8 flex gap-x-1 flex-col text-center">
+      <div className="fixed bottom-8 left-1/2 -translate-x-1/2 -translate-y-1/2 flex gap-x-1 flex-col text-center w-full min-w-small">
         <p>Made by:</p>
         <p className="font-bold made-by-text">Tobias Tranberg Viskum</p>
       </div>
