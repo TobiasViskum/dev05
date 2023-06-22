da990046779856a3652e943032e887d074bf65fb
TobiasViskum <tobias.t.viskum@gmail.com>
2023-06-22 18:50:27 +0200

    commit

--- src/components/page-index/SpinningIcon.tsx
+++ src/components/page-index/SpinningIcon.tsx
@@ -3,7 +3,7 @@ import { LoadingSpinner } from "../global";
 export default function SpinningIcon() {
   return (
     <>
-      <LoadingSpinner />
+      <LoadingSpinner opacity="opacity-100" />
       <div className="fixed bottom-8 left-1/2 -translate-x-1/2 -translate-y-1/2 flex gap-x-1 flex-col text-center w-full min-w-small">
         <p>Made by:</p>
         <p className="font-bold made-by-text">Tobias Tranberg Viskum</p>
